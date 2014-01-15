50286ce2761d89133d87988b801f4e8d817b3ba8
Stepler Paulo <paulobiscocho@gmail.com>
2014-01-15 15:12:13 +0800

    Daily Inventory Sheet Hotmeal Adjusted
    Set meal Issue Solved

--- models/daily_menu.php
+++ models/daily_menu.php
@@ -23,24 +23,43 @@ class DailyMenu extends AppModel {
 	public function daily_inventory_sheet_hotmeal($date){
 		return $this->query( 
 		"SELECT 
-		  `sale_details`.`item_code`,
-		  `menu_items`.`name`,
-		  SUM(`sale_details`.`qty`) AS no_of_sold_item,
-		  `daily_menus`.`approx_srv`,
-		  `daily_menus`.`selling_price` 
-		FROM
-		  `canteen`.`sale_details` 
-		  INNER JOIN `canteen`.`menu_items` 
-			ON (
-			  `sale_details`.`item_code` = `menu_items`.`item_code`
-			) 
-		  INNER JOIN `canteen`.`daily_menus` 
-			ON (
-			  `menu_items`.`id` = `daily_menus`.`menu_item_id`
-			) 
-		WHERE `sale_details`.`created` >= '$date' 
-			AND `sale_details`.`is_setmeal_hdr`  = '0'
-		GROUP BY `sale_details`.`item_code`" 
+			  `sale_details`.`item_code`,
+			  `menu_items`.`name` AS menu_item_name,
+			  `products`.`name` AS product_name,
+			  SUM(`sale_details`.`qty`) AS no_of_sold_item,
+			  `daily_menus`.`approx_srv`,
+			  `daily_menus`.`additional_approx_srv`,
+			  `daily_menus`.`selling_price`,
+			  `daily_menus`.`srv_left`,
+			  `sale_details`.`is_setmeal_hdr`,
+			  `sale_details`.`is_setmeal_dtl` 
+			FROM
+			  `sale_details` 
+			  LEFT OUTER JOIN `products` 
+				ON (
+				  `products`.`item_code` = `sale_details`.`item_code`
+				) 
+			  LEFT OUTER JOIN `menu_items` 
+				ON (
+				  `sale_details`.`item_code` = `menu_items`.`item_code`
+				) 
+			  LEFT OUTER JOIN `daily_menus` 
+				ON (
+				  `menu_items`.`id` = `daily_menus`.`menu_item_id`
+				) 
+				AND (
+				  DATE(`daily_menus`.date) = DATE(`sale_details`.created)
+				) 
+			WHERE `sale_details`.`created` >= '$date' 
+			AND   NOT (`sale_details`.`is_setmeal_hdr`=0 AND 
+			  `sale_details`.`is_setmeal_dtl` =0 AND
+			   `menu_items`.`name`  IS NULL)
+			GROUP BY `sale_details`.`item_code`,
+			  `sale_details`.`is_setmeal_dtl`,
+			  `sale_details`.`is_setmeal_hdr` 
+			ORDER BY `sale_details`.`created`,
+			  `sale_details`.`is_setmeal_dtl`,
+			  `sale_details`.`is_setmeal_hdr` " 
 		);
 	}
 }
