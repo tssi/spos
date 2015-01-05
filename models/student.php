a6d1d56d995351b32207609a8594bf9cce72d1c4
Paulo <paulobiscocho@gmail.com>
2015-01-05 15:48:16 +0800

    Student Model Adjusted

--- models/student.php
+++ models/student.php
@@ -2,6 +2,6 @@
 class Student extends AppModel {
 	var $name = 'Student';
 	var $virtualFields = array(
-			'FullName'=>"CONCAT(Student.LastName, ', ',Student.FirstName, '  ')"
+			'full_name'=>"CONCAT(Student.LastName, ', ',Student.FirstName, '  ')"
 		);
 }
